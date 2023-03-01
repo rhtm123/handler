736b085c90a9537992fb5dbd0cfc1d397b85c40a
rhtm123 <maurya.iitk@gmail.com>
2023-03-01 15:08:01 +0530

    socket code added

--- main.py
+++ main.py
@@ -98,19 +98,19 @@ def get_data(data):
     print(code)
     print(container_name)
     print(file_name)
-    # with open("code/main.py", "w") as f:
-    #     f.write(code)
+    with open("code/main.py", "w") as f:
+        f.write(code)
 
-    # with open("tmp/output.txt", "w") as output:
-    #     subprocess.run(f"sudo docker cp code/main.py {container_name}:{file_name}", shell=True, stdout=output, stderr=output)
+    with open("tmp/output.txt", "w") as output:
+        subprocess.run(f"sudo docker cp code/main.py {container_name}:{file_name}", shell=True, stdout=output, stderr=output)
 
-    # with open("tmp/output.txt", "r") as file:
-    #     val = file.read()
+    with open("tmp/output.txt", "r") as file:
+        val = file.read()
 
-    val = "fdaf"
+    # val = "fdaf"
     d = {"success":True, "container_name":container_name, "response":val}
     # return jsonify(d)
-    emit("data",jsonify(d))
+    emit("data",d)
 
 @socketio.on("connect")
 def connect():
